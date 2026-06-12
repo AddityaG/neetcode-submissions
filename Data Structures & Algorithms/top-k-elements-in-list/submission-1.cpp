b7a0123a570b04170ef75e117f14fa6c982202a9
AddityaG <75774541+AddityaG@users.noreply.github.com>
2026-06-12 07:13:32 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for(int i=0;i<nums.size();i++){
+            if(count.find(nums[i])!=count.end()){
+                count[nums[i]]+=1;
+            }
+            else{
+                count.insert({nums[i],1});
+            }
+        }
+        vector<pair<int,int>> countPair;
+        for(auto it: count){
+            cout<<it.second<<" "<<it.first<<endl;
+            countPair.push_back({it.second,it.first});
+        }
+        sort(countPair.begin(),countPair.end());
+        vector<int> ans;
+        for(int i=countPair.size()-1; i > (int)countPair.size() - 1 - k; i--){
+            ans.push_back(countPair[i].second);
+        }
+        return ans;
+    }
+};
