9f695e5481be16a73f10ca6eff086b000d047fee
AddityaG <75774541+AddityaG@users.noreply.github.com>
2026-07-06 07:07:47 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0, r = nums.size();
+        int x = -1;
+        while(l<r && (r-l)>1){
+            int x = (l+r)/2;
+            cout<<"x: "<<x<<endl;
+            if(target == nums[x]) return x;
+            if(target > nums[x]) l = x;
+            if(target < nums[x]) r = x;
+        }
+        if(l>=0){
+            if(target==nums[l]) return l;
+        }
+        if(r<nums.size()){
+            if(target==nums[r]) return r;
+        }
+        return -1;
+    }
+};
